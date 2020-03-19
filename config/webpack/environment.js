016a1026e859c27e69a68e0aed8707c474cc9f44
tojio <firebirdv.toji.fv5cl7@gmail.com>
2020-03-19 19:48:34 +0900

    seedファイル作成途中まで

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+$: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+  )
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,85 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+item.create(
+  name: "お店1"
+  type: "和食"
+  price: 1500
+  closing_day: "月曜日"
+  open_time: 11:30
+  close_time: 22:00
+  website: なし
+  photo1:
+  photo2:
+  photo3:
+  beji: 0
+  tel: 0123-111-111
+  parking: 1
+  address: "八幡平市スパルタ1-2-3"
+)
+
+item.create(
+  name: "お店2"
+  type: "洋食"
+  price: 3250
+  closing_day: "水曜日"
+  open_time: 11:00
+  close_time: 21:00
+  website: https://abcdefg
+  photo1:
+  photo2:
+  photo3:
+  beji: 1
+  tel: 0123-222-222
+  parking: 1
+  address: "八幡平市スパルタ2-2-2"
+)
+
+
+item.create(
+  name: "お店3"
+  type: "軽食"
+  price: 1500
+  closing_day: "水曜日"
+  open_time: 12:00
+  close_time: 16:00
+  website: https://abcdefg
+  photo1:
+  photo2:
+  photo3:
+  beji: 1
+  tel: 0123-333-333
+  parking: 0
+  address: "八幡平市スパルタ3-3-3"
+)
+
+item.create(
+  name: "お店4"
+  type: "焼き肉"
+  price: 3250
+  closing_day: "月曜日"
+  open_time: 17:00
+  close_time: 00:00
+  website: "https://abcdefg"
+  photo1:
+  photo2:
+  photo3:
+  beji: 0
+  tel: 0123-444-444
+  parking: 1
+  address: "八幡平市スパルタ4-4-4"
+)
+
+item.create(
+  name: "お店5"
+  type: "ラーメン"
+  price: 2000
+  closing_day: "なし"
+  open_time: 8:00
+  close_time: 1:00
+  website: https://abcdefg
+  photo1:
+  photo2:
+  photo3:
+  beji: 0
+  tel: 0123-444-444
+  parking: 1
+  address: "八幡平市スパルタ5-5-5"
+)
