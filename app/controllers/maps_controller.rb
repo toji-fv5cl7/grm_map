2141791f7364f2e065e6c33eac58b721865ac842
tantan <tantant5656@gmail.com>
2020-03-19 13:39:15 +0900

    show編集、controller編集

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -3,11 +3,11 @@ class MapsController < ApplicationController
     end
 
     def new
-      @map = map.new # フォーム用の空のインスタンスを生成する。
+      @map = Map.new # フォーム用の空のインスタンスを生成する。
     end
 
     def create
-      @map = map.new(map_params) # ストロングパラメータを引数に
+      @map = Map.new(map_params) # ストロングパラメータを引数に
       if @map.save
         redirect_to @map, notice: "投稿を登録しました。"
       else
@@ -16,14 +16,14 @@ class MapsController < ApplicationController
     end
 
     def show
-      @map = map.find(params[:id])
+      @map = Map.find(params[:id])
     end
 
   def edit
-    @map = map.find(params[:id])
+    @map = Map.find(params[:id])
   end
   def update
-    @map = map.find(params[:id])
+    @map = Map.find(params[:id])
     if @map.update(map_params)
       redirect_to @map, notice: "投稿を更新しました。"
     else
